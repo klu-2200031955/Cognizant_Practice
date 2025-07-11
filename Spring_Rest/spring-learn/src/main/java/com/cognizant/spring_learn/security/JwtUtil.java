7caa12769e5bb57546c51d2bc51cf88854fc8b78
Samudrala Venkata Pavan Tarun Kumar <2200031955@kluniversity.in>
2025-07-11 21:12:10 +0530

    Uploaded Week 4 HandsOn

---
+++ Spring_Rest/spring-learn/src/main/java/com/cognizant/spring_learn/security/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.cognizant.spring_learn.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRATION_TIME = 10 * 60 * 1000; 
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+}
---
+++ Spring_Rest/spring-learn/src/main/java/com/cognizant/spring_learn/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.cognizant.spring_learn.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/authenticate").permitAll()
+	            .anyRequest().authenticated()
+	        );
+	    return http.build();
+	}
+}
